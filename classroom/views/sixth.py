b28bc2f827f524e56bfce9e5d95908d92ae9ac5e
Dominic Canora <dominiccanora@gmail.com>
2021-10-26 21:45:18 -0400

    Make 6th assignment tracker keep running total

--- classroom/views/sixth.py
+++ classroom/views/sixth.py
@@ -7,7 +7,6 @@ from classroom.models import Student, Submission
 
 def tracker(request, group):
     students = []
-    last_week = timezone.now().replace(hour=0, minute=0, second=0) - timezone.timedelta(days=7)
     q = Student.objects.filter(homeroom=group).order_by("lname")
 
     for stu in q:
@@ -16,15 +15,13 @@ def tracker(request, group):
         incomplete = 0
         group = stu.get_homeroom_display()
 
-        subs = Submission.objects.filter(student=stu, submitted_at__gte=last_week)
+        subs = Submission.objects.filter(student=stu)
         for sub in subs:
             if sub.satisfactory != False:
                 stars += sub.assignment.name.count("⭐")
                 extra_stars += sub.assignment.name.count("✴️")
-            else:
-                incomplete += 1
 
-        students.append((stu, range(stars), range(extra_stars), range(incomplete)))
+        students.append((stu, stars, extra_stars, incomplete))
 
     return render(request, "classroom/sixth_tracker.html", {
         "students": (students[:(len(students)+1)//2], students[(len(students)+1)//2:]),

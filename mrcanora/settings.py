6db631d417875c92b399632cc129078d5390f226
Dominic Canora <dominiccanora@gmail.com>
2022-08-11 14:40:53 -0400

    Update course ID

--- mrcanora/settings.py
+++ mrcanora/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = not os.getenv("PROD", False)
 DOCKER = os.getenv("DOCKER", False)
-SIXTH_COURSE_ID = 9606
+SIXTH_COURSE_ID = 11134
 OTHER_COURSE_IDS = []
 
 

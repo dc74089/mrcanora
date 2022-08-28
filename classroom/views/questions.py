911564f7dab24f1c97e32b20a0a65177d56be2da
Dominic Canora <dominiccanora@gmail.com>
2022-08-28 17:11:33 -0400

    Add percentage of all students to question answer view

--- classroom/views/questions.py
+++ classroom/views/questions.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from django.http.response import HttpResponseBadRequest, HttpResponseForbidden
 from django.shortcuts import redirect, render
 
@@ -75,18 +77,29 @@ def view(request):
         ctx['question'] = q
 
         ans = TeambuildingResponse.objects.filter(question__id=q.id)
+        ctx['total'] = ans.count()
+
         out = {}
+
+        for a in ans:
+            if a.answer not in out:
+                out[a.answer] = {}
+                out[a.answer]['count'] = 0
+                out[a.answer]['students'] = []
+
+            out[a.answer]['count'] += 1
 
         if 'homeroom' in request.GET:
             ans = ans.filter(student__homeroom=request.GET['homeroom']) | ans.filter(student__id="dc74089")
 
         for a in ans:
-            if a.answer not in out:
-                out[a.answer] = []
+            out[a.answer]['students'].append(a)
 
-            out[a.answer].append(a)
+        for a in out:
+            out[a]['percentage'] = round(out[a]['count'] / ctx['total'] * 100)
 
         ctx['answers'] = out
+
         if len(out) > 0:
             ctx['coltype'] = "col-lg-" + str(12 // len(out)) if len(out) <= 4 else "col-lg-3"
 
